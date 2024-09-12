60a0bf8682224ef0eb5fdb0404808817b6d87658
Nathan Walton <nathan@waltonwebservices.com>
2024-09-12 19:42:59 +0100

    update exec script

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-	testEnvironment: "node",
-	transform: {
-	  "^.+.tsx?$": ["ts-jest", {}],
-	},
-	roots: ["<rootDir>/src/tests"],
-	testMatch: ["<rootDir>/src/tests/**/*.(spec|test).[jt]s?(x)"],
-  };
